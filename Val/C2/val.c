3906c0c820e290a680d7293c413bcc64b972a33c
rodrigolinhas <rodrigolinhas18@gmail.com>
2025-03-23 10:30:06 +0000

    SMALL PATCH AND FIXED TYPOS

--- Val/C2/val.c
+++ Val/C2/val.c
@@ -1,11 +1,12 @@
 #include <stdio.h> //printf
-#include <stdlib.h> // mallor, exit
+#include <stdlib.h> // malloc, exit
 #include <string.h> // strlen
 #include <errno.h> // erro handler
 #include <sys/types.h> // pid_t
 #include <sys/wait.h> // waitpid
 #include <unistd.h> // fork, pipe, read, write, close
-#include <fcntl.h>  //open, close
-//#include <time.h>
+#include <fcntl.h>  //open
+//#include <time.h> //srand(time) (para gerar varias seeds no random)
 #define R 0 //read pipe
-#define W 1 //wirte pipe
+#define W 1 //write pipe
+
