3194a89909fb1405b27f9cd536d3eb8958ed0123
rodrigolinhas <rodrigolinhas18@gmail.com>
2025-03-28 19:26:10 +0000

    added missing theory from previous class

--- TEORICA/race-condition.c
+++ TEORICA/race-condition.c
@@ -1,40 +1,51 @@
-
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
+#include <errno.h>
 
 pthread_mutex_t mutexFuel;
 pthread_cond_t condFuel;
 int fuel = 0;
 
-void* fuel_filling(void* arg){
-    for (int i = 0; i < 5; i++){
+void* fuel_filling(void* arg) {
+    for (int i = 0; i < 5; i++) {
         pthread_mutex_lock(&mutexFuel);
         fuel += 15;
-        printf("Filled fuel ... %d\n", fuel);
+        printf("Filled fuel... %d\n", fuel);
         pthread_mutex_unlock(&mutexFuel);
         pthread_cond_signal(&condFuel);
         sleep(1);
     }
 }
 
-void* car(void*){
-    pthread_mutex_unlock(&mutexFuel);
-    while (fuel < 40){
-        printf("No fuel, waiting ...\n");
-        pthread_cond_wait(&condFuel, &mutexFuel);
+void* car(void* arg) {
+    pthread_mutex_lock(&mutexFuel);
+    while (fuel < 40) {
+        printf("No fuel. Waiting...\n");
+       pthread_cond_wait(&condFuel, &mutexFuel);
+        // Equivalent to:
+        // pthread_mutex_unlock(&mutexFuel);
+        // wait for signal on condFuel
+        // pthread_mutex_lock(&mutexFuel);
     }
     fuel -= 40;
-    printf("Got fuel, driving ...");
+    printf("Got fuel. Now left: %d\n", fuel);
     pthread_mutex_unlock(&mutexFuel);
 }
- 
-//REVIEW
-int main(int argc, char const *argv[])
-{
-    pthread_t thread[2];
+
+int main(int argc, char* argv[]) {
+    pthread_t th[2];
     pthread_mutex_init(&mutexFuel, NULL);
     pthread_cond_init(&condFuel, NULL);
-    return 0;
-}
+    for (int i = 0; i < 2; i++) {
+        if (i == 1) {
+            if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0) {
+                perror("Failed to create thread");
+            }
+        } else {
+            if (pthread_create(&th[i], NULL, &car, NULL) != 0) {
+                perror("Failed to create thread");
+            }
+        }
+    }
