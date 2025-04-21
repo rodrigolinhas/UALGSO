ad835bac51a4ba55a423c150db78c78ed6ca530b
Rodrigo Linhas <rodrigolinhas18@gmail.com>
2025-04-21 19:57:17 +0100

    added folder
    Nada de especial, hoje so revi videos no youtube sobre isto.
    Muito bom o canal do CodeVault

--- Val/C3/val.c
+++ Val/C3/val.c
@@ -1,4 +1,8 @@
-#include <stdio.h>  
+/*
+Teste 1 2 3 
+*/
+
+#include <stdio.h>
 #include <stdlib.h>  
 #include <pthread.h>  
 #include <signal.h>
@@ -6,10 +10,10 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include  <fcntl.h>
+#include <fcntl.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+	
+    	return 0;
 }
