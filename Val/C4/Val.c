f04d3849b6796b916b2920765fb0e53dc9118352
Rodrigo Linhas <rodrigolinhas18@gmail.com>
2025-05-12 15:54:02 +0100

    update Val.c

---
+++ Val/C4/Val.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <semaphore.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
